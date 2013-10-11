a5683ad71e280561a725322d61b42244b3ae6ffe
Tom Buchok <tom@buchok.com>
2013-10-11 09:39:45 +0200

    refactors api into lib module

--- api.js
+++ api.js
@@ -1,27 +1,25 @@
 var http = require('http')
   , timeout = Number(process.argv[2] || 0)
-  , url = 'http://localhost:8000/delay/' + timeout.toFixed(1);
+  , API = new require('./lib/api')
+  , api = new API(timeout)
 ;
 
-http.createServer(function(req, res) {
-  var server = this;
-  setTimeout(function() {
-    res.end('Lorem ipsum dolor sit amet.');
-    server.close();
-  }, timeout * 1000);
-}).listen(8000);
+api.start();
 
-console.log('API: Hitting: %s \n', url);
-http.get(url, function(res) {
+console.log('API: Hitting: %s \n', api.url());
+http.get(api.url(), function(res) {
   res
-  .on('error', function(err) { console.error(err) })
-  .on('end', function() { console.log('\nAPI: Ended!') })
-  .pipe(process.stdout)
+    .on('error', function(err) { console.error(err) })
+    .on('end', function() { console.log('\nAPI: Ended!') })
+    .pipe(process.stdout)
   ;
 });
 
 // We are not blocked, we can do other things:
 console.log('OTHER: Do other stuff while we wait ... \n');
 console.log('OTHER: 1 + 1: %s', 1 + 1);
-console.log('OTHER: Circle area with radius `4`: %s', Math.PI * Math.pow(2, 4));
+console.log('OTHER: Circle area with radius `4`: %s'
+            , Math.PI * Math.pow(2, 4));
 console.log('\n');
+
+
