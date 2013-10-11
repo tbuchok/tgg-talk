5629b85589fb59946f3e2a9dd0f654167b80451f
Tom Buchok <tom@buchok.com>
2013-10-11 08:34:01 +0200

    adds messaging

--- api.js
+++ api.js
@@ -3,11 +3,17 @@ var http = require('http')
   , url = 'http://slowapi.com/delay/' + timeout.toFixed(1);
 ;
 
+console.log('API: Hitting: %s \n', url);
 http.get(url, function(res) {
   res
-  .on('end', function() { console.log('\nEnded!') })
+  .on('error', function(err) { console.error(err) })
+  .on('end', function() { console.log('\nAPI: Ended!') })
   .pipe(process.stdout)
   ;
 });
-console.log('Hitting ' + url);
-console.log('Do other stuff while we wait ... ');
+
+// We are not blocked, we can do other things:
+console.log('OTHER: Do other stuff while we wait ... \n');
+console.log('OTHER: 1 + 1: %s', 1 + 1);
+console.log('OTHER: Circle area with radius 4: %s', Math.PI * Math.pow(2, 4));
+console.log('\n');
